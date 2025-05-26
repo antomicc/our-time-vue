50624109395834ae997376bd7edbd0225b09b054
antomic <ruizm.aj@gmail.com>
2025-05-26 12:04:21 -0600

    Linea completa falta fotos

--- src/data.ts
+++ src/data.ts
@@ -179,8 +179,289 @@ export const memories = [
     id: '13',
     date: '4 Abril, 2021',
     title: 'Nuestro primer viaje juntos',
-    description: 'Fuimos sin planear mucho con mi familia, tu y yo hablamos de lo sentimos.',
+    description: 'Fuimos sin planear mucho con mi familia, tu y yo hablamos de lo que sentimos.',
     location: 'Bonanza La Ponderosa',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '14',
+    date: '2 Enero, 2022',
+    title: 'Empezando el año juntos',
+    description:
+      'Aun hay secuelas de la pandemia, pero empezamos el año juntos y con muchas ganas.',
+    location: 'Irtra Petapa',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '15',
+    date: '18 Enero, 2022',
+    title: 'Primer viaje, solo nosotros',
+    description:
+      'Es la primera ves que viajamos solos, sin familia, solo nosotros dos. Fue un viaje corto pero nos conectamos bastante y nos llenamos de amor.',
+    location: 'Bee Cabañas',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '16',
+    date: 'Siempre',
+    title: 'Cool',
+    description:
+      'Tu aura es tan fresca y relajante, siempre me haces sentir bien cuando estoy contigo.',
+    location: 'En cualquier lugar',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '17',
+    date: 'Siempre',
+    title: 'Un clasico',
+    description:
+      'Tu y yo tomando café, hablando de la vida es siempre un clásico que nunca pasa de moda.',
+    location: 'Todos los lugares',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '18',
+    date: '14 Agosto, 2022',
+    title: 'Una comida que nunca volveras a probar',
+    description:
+      'La comida estaba bien, el ambiente era agradable, pero tu torito no era un torito.',
+    location: 'Restaurante en Miraflores',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '19',
+    date: '11 Diciembre, 2022',
+    title: '7 Años Juntos',
+    description: 'Celebramos tarde nuestro aniversario, pero lo hicimos con mucho amor y alegría.',
+    location: 'Irtra, de noche',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '20',
+    date: '1 Enero, 2023',
+    title: 'Celebrando el nuevo año',
+    description:
+      'Empezamos otro año juntos, con muchas ganas de seguir creciendo y aprendiendo juntos.',
+    location: 'En Casa',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '21',
+    date: '16 Agosto, 2023',
+    title: 'Segunda Feria',
+    description: 'Volvimos a la feria de Jocotenango, esta vez con más experiencia y más amor.',
+    location: 'Feria de Jocotenango',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '22',
+    date: '16 Agosto, 2023',
+    title: 'Segunda Feria',
+    description: 'Volvimos a la feria de Jocotenango, esta vez con más experiencia y más amor.',
+    location: 'Feria de Jocotenango',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '23',
+    date: '14 Noviembre, 2023',
+    title: 'Viendo las estrellas',
+    description: 'Estamos cumpliendo 8 años juntos y celebramos viendo las estrellas.',
+    location: 'El Barranco',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '24',
+    date: '19 Marzo, 2024',
+    title: 'La playa',
+    description:
+      'Fuimos a la playa por primera vez juntos, fue un momento mágico lleno de risas y amor.',
+    location: 'Puerto Chulamar',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '25',
+    date: '12 Mayo, 2024',
+    title: 'Primer Concierto',
+    description:
+      'Fuimos a nuestro primer concierto juntos, fue una experiencia inolvidable llena de música',
+    location: 'Puerto Chulamar',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '26',
+    date: '29 Agosto, 2024',
+    title: 'Tu cumpleaños a lo italiano',
+    description:
+      'En tu cumpleaños, decidimos celebrar a lo italiano, con una cena especial en un rincon bonito.',
+    location: 'Restaurante Dolcelato',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '27',
+    date: '20 Octubre, 2024',
+    title: 'Tu y yo',
+    description:
+      'Cerca de mi cumpleaños, recuerdo que tu y yo somos más que una pareja, estamos conectados de una manera especial.',
+    location: 'En todos los lugares',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '28',
+    date: '16 Noviembre, 2024',
+    title: 'Santa Teresita',
+    description:
+      'Hemos ido a Santa Teresita, un lugar bonito para relajarnos y disfrutar de la vista.',
+    location: 'Santa Teresita, Amatitlán',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '29',
+    date: '25 Diciembre, 2024',
+    title: 'Primera Navidad juntos',
+    description:
+      'Ha sido nuestra primera Navidad juntos, disfrutamos de la cena, la familia y estar juntos.',
+    location: 'En tu casa',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '30',
+    date: '25 Diciembre, 2024',
+    title: 'Primera Navidad juntos',
+    description:
+      'Ha sido nuestra primera Navidad juntos, disfrutamos de la cena, la familia y estar juntos.',
+    location: 'En tu casa',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '31',
+    date: '8 Febrero, 2025',
+    title: 'Viaje a Antigua',
+    description:
+      'No es el mejor viaje que hemos hecho, pero es gracioso recordar los momentos que pasamos juntos, aun asi tiene unas de las mejores vistas.',
+    location: 'Altamira, Antigua Guatemala',
+    imageUrl:
+      'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    song: {
+      title: 'Home',
+      artist: 'Michael Bublé',
+      url: 'https://open.spotify.com/track/2s9QWMzOF3AhRCvmJcavDJ',
+    },
+  },
+  {
+    id: '32',
+    date: '30 Marzo, 2025',
+    title: 'Siddharta',
+    description:
+      'Nunca te habia visto tan feliz, el concierto de Siddharta fue un momento especial, lleno de música y emoción.',
+    location: 'Parque de la Industria',
     imageUrl:
       'https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     song: {
