96d1c809558f7c12221977978bded69f47de8027
antomic <ruizm.aj@gmail.com>
2025-07-09 17:22:45 -0600

    Ajustes de Config de Vite y Package

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/our-time-vue/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/our-time-vue/',
 })
